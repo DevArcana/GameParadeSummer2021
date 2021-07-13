564c12c551b211845058d24281dc52fa1c698dcf
Piotr Krzystanek <piotr.krzystanek@gmail.com>
2021-07-13 20:45:22 +0200

    add action points display

---
+++ Assets/Scripts/Interface/ActionPointsUI.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Interface
+{
+    public class ActionPointsUI : MonoBehaviour
+    {
+        public Toggle actionPoint1;
+        public Toggle actionPoint2;
+
+        private void Start()
+        {
+            actionPoint1.isOn = false;
+            actionPoint2.isOn = false;
+        }
+    }
+}
