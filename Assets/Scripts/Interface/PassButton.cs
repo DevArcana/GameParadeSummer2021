3ffdcbfc9e06396f08f427ea342b4b13b4c55b22
Dasarro <wicia0202@wp.pl>
2021-07-18 21:41:57 +0200

    Add pass turn button

---
+++ Assets/Scripts/Interface/PassButton.cs
@@ -0,0 +1,22 @@
+﻿using Arena;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Interface
+{
+    public class PassButton : MonoBehaviour
+    {
+        public Button passButton;
+        private void Start()
+        {
+            var turnManager = TurnManager.Instance;
+            passButton.onClick.AddListener(() => { turnManager.NextTurn(); });
+            turnManager.TurnStarted += OnTurnChange;
+        }
+        
+        private void OnTurnChange(object sender, TurnManager.OnTurnChangeEventArgs args)
+        {
+            passButton.interactable = args.Entity is PlayerEntity;
+        }
+    }
+}
